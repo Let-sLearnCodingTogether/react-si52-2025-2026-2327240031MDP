8d42c43cbde510b8499b4f23cfa151521f3232b6
shela-2226250087 <vicinskyowentanner_2327240031@mhs.mdp.ac.id>
2025-11-12 20:33:12 +0700

    Movies Ops

---
+++ src/pages/movies/Movies.tsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router"
+
+function Movies() {
+    return <div>
+        <h2>
+            Movie Page
+        </h2>
+        <NavLink to="/add-movie" className="btn btn-primary"> Add Movies </NavLink>
+    </div>
+}
+
+export default Movies
