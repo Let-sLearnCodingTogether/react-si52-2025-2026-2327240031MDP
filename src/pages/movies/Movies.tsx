bb1f4926d1bda1dce8468c94e3533683d07a1f04
2327240031MDP <vicinskyowentanner_2327240031@mhs.mdp.ac.id>
2025-11-15 18:40:42 +0700

    Movies Axios add

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -1,11 +1,36 @@
+import { useCallback, useEffect, useState } from "react"
 import { NavLink } from "react-router"
+import ApiClient from "../../utils/ApiClient"
+
+interface Movie {
+    _id : string,
+    judul : string,
+    taunRilis : string,
+    sutradara : string,
+    createdAt : string,
+    updatedAt : string
+}
 
 function Movies() {
-    return <div>
-        <h2>
-            Movie Page
-        </h2>
-        <NavLink to="/add-movie" className="btn btn-primary"> Add Movies </NavLink>
+    const [movies, setMovies] = useState<Movie[]>([])
+
+    const fetchMovies = useCallback(async () => {
+        const response = await ApiClient.get("/movies")
+
+        if(response.status == 200) {
+            setMovies(response.data.data)
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchMovies()
+    }, [fetchMovies])
+
+    return <div className="container mx-auto">
+        <div className ="d-flex justify-content-between mb-3">
+            <h2> Movie Page </h2>
+            <NavLink to="/add-movie" className="btn btn-primary"> Add Movies </NavLink>
+        </div>
     </div>
 }
 
